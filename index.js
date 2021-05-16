1aec98f9ab22217cc72176ff07112126599b0234
alex <fancn21th@aliyun.com>
2021-05-16 12:26:55 +0800

    hello story-blok

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// 1. Require the Storyblok client
+const StoryblokClient = require("storyblok-js-client");
+
+// 2. Initialize the client with the preview token
+// from your space dashboard at https://app.storyblok.com
+let Storyblok = new StoryblokClient({
+  accessToken: "pHoqjoiXRms6gRAJpDcTJQtt",
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  Storyblok.get("cdn/stories/home", {
+    version: "draft",
+  })
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(port, () => {
